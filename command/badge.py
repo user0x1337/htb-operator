dc4b75435798f67633422f98aad22cd6ead5cfd9
user0x1337 <user0x1337@protonmail.com>
2024-12-15 07:56:02 +0100

    added draft for badge command

--- command/badge.py
+++ command/badge.py
@@ -5,6 +5,7 @@ from command.base import BaseCommand
 
 class BadgeCommand(BaseCommand):
 
+    # noinspection PyUnresolvedReferences
     def __init__(self, htb_cli: "HtbCLI", args: argparse.Namespace):
         super().__init__(htb_cli=htb_cli, args=args)
 
