7d1f4ee035decd636c80b3cef18428a224871e13
user0x1337 <user0x1337@protonmail.com>
2024-12-18 13:48:30 +0100

    version: 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="htb-operator",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     py_modules=["htb_operator"],
     install_requires=[
