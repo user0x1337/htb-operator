c4dc159c5f59c43da892cd3df6e400bb6f1cef10
user0x1337 <user0x1337@protonmail.com>
2024-12-18 15:33:08 +0100

    fixed

--- htb_operator.py
+++ htb_operator.py
@@ -79,7 +79,7 @@ class HtbCLI:
         if IS_ROOT_OR_ADMIN:
             self.logger.warning(f'{Fore.LIGHTYELLOW_EX}App is running as {"Administrator" if IS_WINDOWS else "root"}{Style.RESET_ALL}')
 
-fix        parser: ArgumentParser = create_arg_parser(self)
+        parser: ArgumentParser = create_arg_parser(self)
         args: argparse.Namespace = parser.parse_args()
         init = self.api_key is not None
 
